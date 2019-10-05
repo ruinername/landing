81d607b0f6f1205f1ad9ae635dd726a9cf570cbb
Max Voloshinskii <me@voloshinskii.ru>
2019-10-05 16:47:36 +0300

    Updates

---
+++ precache-manifest.baec0dc7bd7e544f69f8b5658661108f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac780c321c91aad3df4c97a7be98786c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9bca263a47b9cfa00abb",
+    "url": "/static/css/main.f14eab08.chunk.css"
+  },
+  {
+    "revision": "457d7de7067e816a8017",
+    "url": "/static/js/2.7ba164f5.chunk.js"
+  },
+  {
+    "revision": "9bca263a47b9cfa00abb",
+    "url": "/static/js/main.70658a5d.chunk.js"
+  },
+  {
+    "revision": "262ea7edc450f594219b",
+    "url": "/static/js/runtime-main.e7f0abef.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
